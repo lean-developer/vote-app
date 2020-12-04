ac479db063a48a57bf5625778c6a9be70a832146
Ingo Cloß <ingo.clohs@icloud.com>
2020-12-04 16:11:04 +0100

    init commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,21 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
-  </div>
+    <b-container>
+      <b-card
+        title="Neue Abstimmung"
+        img-src="https://picsum.photos/600/300/?image=25"
+        img-alt="Image"
+        img-top
+        tag="article"
+        style="max-width: 20rem;"
+        class="mb-2"
+      >
+        <b-card-text>
+          <b-input type="text" v-model="abstimmung"></b-input>
+        </b-card-text>
+
+        <b-button href="#" variant="primary" @click="onStartVote()">Abstimmung starten</b-button>
+      </b-card>
+  </b-container>
 </template>
 
 <script lang="ts">
@@ -11,8 +24,14 @@ import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
 
 @Component({
   components: {
-    HelloWorld,
   },
 })
-export default class Home extends Vue {}
+export default class Home extends Vue {
+  private abstimmung: string = ''
+
+  onStartVote() {
+    console.log('VOTE', this.abstimmung);
+    this.$router.push({ name: 'Vote', params: { voteId: "1" } })
+  }
+}
 </script>
