da14914d4d603cf7d32d55e54d9ed3e1038d8898
Ingo Cloß <ingo.clohs@icloud.com>
2020-12-15 16:39:47 +0100

    add auth

--- src/domain/api/server/http.ts
+++ src/domain/api/server/http.ts
@@ -4,6 +4,7 @@ export default axios.create({
     baseURL: 'https://icl-vote-service.herokuapp.com/',
     // baseURL: 'http://localhost:3200/',
     headers: {
-      'Content-Type': 'application/json'
+      'Content-Type': 'application/json',
+      'Authorization': 'Basic YnVrb3dza2k6XzVmRjcjej0/Jlo2P21RNkgh'
     }
   })
